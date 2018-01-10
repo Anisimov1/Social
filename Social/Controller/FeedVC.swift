8d3034d7821e866757bb4530f019e5a204ed5e18
Anisimov1 <anisimov_1@aol.com>
2018-01-09 23:04:46 -0500

    Completed Firebase display

--- Social/Controller/FeedVC.swift
+++ Social/Controller/FeedVC.swift
@@ -44,10 +44,15 @@ class FeedVC: UIViewController, UITableViewDelegate,UITableViewDataSource {
         return posts.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
         let post = posts[indexPath.row]
-        print("TONY: \(post.caption)")
         
-        return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as? PostCell {
+            cell.configureCell(post: post)
+            return cell
+        } else {
+            return PostCell()
+        }
     }
     
     @IBAction func signOutPressed(_ sender: Any) {
